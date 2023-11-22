a71d270a61c7e4f07eb1f377105af78be24649b1
Dan Publiweb <91180870+DPubliweb@users.noreply.github.com>
2023-11-22 08:51:21 +0200

    Nathan f app.py

--- app.py
+++ app.py
@@ -113,6 +113,17 @@ def append_to_sheet_combles(data, lastname, firstname, email, utm, zipcode): #, 
     sheet.append_row(row)
     print(f"Adding row with data: {row}")
 
+def append_to_sheet_nathan(data, lastname, firstname, email, utm, zipcode): #, lastname, firstname,
+    # Accédez à la feuille Google par son nom.
+    sheet = client.open("Nathan Fartoukh - Réponses 1").sheet1
+
+    # Convertissez le dictionnaire en une liste pour le garder simple
+    # Vous pouvez personnaliser cet ordre selon la structure de votre feuille.
+    row = [data['msisdn'], data['text'], data['message-timestamp'],firstname, lastname, zipcode ,email, utm ] 
+    # Ajoutez les données à la dernière ligne
+    sheet.append_row(row)
+    print(f"Adding row with data: {row}")
+
 def append_to_sheet_viager(data, lastname, firstname, email, utm, zipcode): #, lastname, firstname,
     # Accédez à la feuille Google par son nom.
     sheet = client.open("Viager JAP - Réponses 1").sheet1
@@ -201,6 +212,12 @@ def phone_exists_in_sheet_1(phone_number):
 def phone_exists_in_sheet_nely(phone_number):
     # Obtenez toutes les données de la première colonne (index 0)
     worksheet = client.open("Réponses - Nely").sheet1
+    column_data = worksheet.col_values(1) # Si vous utilisez `gspread`
+    return phone_number in column_data
+
+def phone_exists_in_sheet_nathan(phone_number):
+    # Obtenez toutes les données de la première colonne (index 0)
+    worksheet = client.open("Nathan Fartoukh - Réponses 1").sheet1
     column_data = worksheet.col_values(1) # Si vous utilisez `gspread`
     return phone_number in column_data
 
@@ -376,6 +393,9 @@ def inbound_sms():
             elif utm == '08.11.23/offresemploijo/20k/jap':
                 if not phone_exist_in_sheet_jo(phone):
                     append_to_sheet_jo(data, firstname, lastname, email, zipcode, utm)
+            elif  "nathan" in utm :
+                if not phone_exist_in_nathan(phone):
+                    append_to_sheet_nathan(data, firstname, lastname, email, zipcode, utm)
             elif "demarches" in utm:
                 if not phone_exists_in_sheet_demarches(phone):
                     append_to_sheet_demarches(data, firstname, lastname ,email, zipcode, utm )
