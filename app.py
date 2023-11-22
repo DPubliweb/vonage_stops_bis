60139052373226b12a00fd5605e452776085418d
Dan Publiweb <91180870+DPubliweb@users.noreply.github.com>
2023-11-22 15:01:40 +0200

    Update app.py

--- app.py
+++ app.py
@@ -417,12 +417,13 @@ def inbound_sms():
             elif "demarches" in utm:
                 if not phone_exists_in_sheet_demarches(phone):
                     append_to_sheet_demarches(data, firstname, lastname ,email, zipcode, utm )
+           elif "combles/publiweb" in utm:
+                if not phone_exists_in_sheet_combles_publiweb(phone):
+                    append_to_sheet_combles_publiweb(data, firstname, lastname ,email, zipcode, utm )
             elif "combles/jap" in utm:
                 if not phone_exists_in_sheet_combles(phone):
                     append_to_sheet_combles(data, firstname, lastname ,email, zipcode, utm )
-            elif "combles/publiweb" in utm:
-                if not phone_exists_in_sheet_combles_publiweb(phone):
-                    append_to_sheet_combles_publiweb(data, firstname, lastname ,email, zipcode, utm )
+            
         else:
             results = get_data_from_redshift_nely(data['msisdn'])
             if results:
